b27faf452504ad7859e7e7d78d1c4ff2cf3479d4
Crowdbotics <engineering@crowdbotics.com>
2023-04-25 00:11:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-mountain-d-78750",
+  url: "https://solitary-mountain-d-78750.botics.co",
+  api: "https://solitary-mountain-d-78750.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
